bb5bbbaaaaba9d3577fb62bdba01974a122fe69b
Justin P <justinperrycoding@gmail.com>
2020-12-23 13:54:57 -0700

    Initial upload

---
+++ twitter_credentials.py
@@ -0,0 +1,21 @@
+# twitter credentials
+
+consumer_key = "w9mnwqfzO1ZW0xwu5KfyZDp6o"
+consumer_secret = "02L62vfoN7Uzkuvc5UXoooZO6RpFvWYKBIQ4G1FaaNSTZ8vbSX"
+access_token = "1339433009611747328-ohK48PnV0VCvBFLnPKuYxswq99ffUo"
+access_token_secret = "Sp0vozanwqZYgZQiZupYlMGCxXMJ1Y5akoKTEynsKjNqL"
+
+
+# save USER ID tweets in a dictionary object / json file
+#   
+# import keys from this module to other
+#
+#  
+#
+#
+#
+#
+#
+#
+#
+#
